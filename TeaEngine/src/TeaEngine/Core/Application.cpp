59bee234ddb3cc1fd26eaa28aa96829f3cbba568
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-06-22 18:39:14 +0200

    Window Events Added!!

--- TeaEngine/src/TeaEngine/Core/Application.cpp
+++ TeaEngine/src/TeaEngine/Core/Application.cpp
@@ -8,10 +8,16 @@ namespace Tea
     Application::Application()
     {
         m_Window = Window::Create(WindowProps("Tea Engine"));
+        m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
     }
 
     Application::~Application()
     {
+    }
+
+    void Application::OnEvent(Event& e)
+    {
+        TEA_CORE_INFO("{0}", e);
     }
 
     void Application::Run()
