628601e8f733fa94aa6f167eb5a340c9dc481c84
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-06-23 19:03:41 +0200

    Added Pink Background to the App

--- TeaEngine/src/TeaEngine/Core/Application.cpp
+++ TeaEngine/src/TeaEngine/Core/Application.cpp
@@ -28,6 +28,8 @@ namespace Tea
     {
         while (m_Running)
         {
+            glClearColor(1,0,1,1);
+            glClear(GL_COLOR_BUFFER_BIT);
             m_Window->OnUpdate();
         }
     }
