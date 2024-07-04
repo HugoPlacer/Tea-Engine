34dc3f2de8eb6f4538b8500bd1678a7cf8e0fcbe
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-07-04 18:36:23 +0200

    Removed old Logs

--- TeaEngine/src/TeaEngine/Core/EntryPoint.h
+++ TeaEngine/src/TeaEngine/Core/EntryPoint.h
@@ -6,8 +6,6 @@ int main(int argc, const char** argv)
 {
     Tea::Log::Init();
     TEA_CORE_WARN("Initialized Log!");
-    int a = 5;
-    TEA_INFO("Hello! Var={0}", a);
 
     auto app = Tea::CreateApplication();
     app->Run();
