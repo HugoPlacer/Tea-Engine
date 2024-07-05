9b35bce1461e084fe2f0e43007046ac4d8302630
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-07-06 00:18:50 +0200

    First BufferLayout Aproach

--- TeaEngine/src/TeaEngine/Renderer/Buffer.h
+++ TeaEngine/src/TeaEngine/Renderer/Buffer.h
@@ -5,6 +5,39 @@
 
 namespace Tea {
     
+    enum class ShaderDataType
+    {
+        None = 0, Bool, Int, Float, Vec2, Vec3, Vec4, Mat2, Mat3, Mat4
+    };
+
+    template <ShaderDataType T>
+    class BufferAttribute
+    {
+    public:
+        BufferAttribute(std::string& name)
+        {
+            using enum ShaderDataType;
+            switch(T)
+            {
+                case Bool: BufferAttribute(name, 1, 1); break;
+                case Int: BufferAttribute(name, 1, 4); break;
+            }
+        }
+    private:
+    BufferAttribute(std::string& name, int components, int size)
+        : m_Name(name), m_Components(components), m_Size(size), m_Offset(0) {}
+    private:
+        std::string m_Name;
+        unsigned int m_Components;
+        unsigned int m_Size;
+        unsigned int m_Offset;
+    };
+
+    class BufferLayout
+    {
+        BufferLayout(std::initializer_list<BufferAttribute<ShaderDataType::None>> atrributes)
+    };
+
     class VertexBuffer
     {
     public:
