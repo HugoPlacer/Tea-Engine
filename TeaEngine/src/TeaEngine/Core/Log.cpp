f458aa91dc700267f519af97697d9abacdff3d4f
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-07-19 17:26:40 +0200

    Changed TEA Log Flag by CORE

--- TeaEngine/src/TeaEngine/Core/Log.cpp
+++ TeaEngine/src/TeaEngine/Core/Log.cpp
@@ -11,7 +11,7 @@ namespace Tea
     void Log::Init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("TEA");
+        s_CoreLogger = spdlog::stdout_color_mt("CORE");
         s_CoreLogger->set_level(spdlog::level::trace);
 
         s_ClientLogger = spdlog::stdout_color_mt("APP");
