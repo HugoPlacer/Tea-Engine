df6153e103e8ceaeedd107458c9dd6ef4cb6135a
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-08-06 12:22:44 +0200

    Tracy CMakeLists Flags

---
+++ TeaEngine/src/TeaEngine/Core/Profiler.cpp
@@ -0,0 +1,17 @@
+/* #pragma once
+
+#include <new>
+#include "tracy/Tracy.hpp"
+
+void* operator new(std::size_t count)
+{
+	auto ptr = malloc(count);
+	TracyAlloc(ptr, count);
+	return ptr;
+}
+
+void operator delete(void* ptr) noexcept
+{
+	TracyFree(ptr);
+	free(ptr);
+} */
