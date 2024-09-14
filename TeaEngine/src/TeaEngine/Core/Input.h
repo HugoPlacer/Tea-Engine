837d9d6bf79d22cc4a2907316ea6dd627609e54c
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-09-14 21:33:42 +0200

    Input Class Doxygen Comments

--- TeaEngine/src/TeaEngine/Core/Input.h
+++ TeaEngine/src/TeaEngine/Core/Input.h
@@ -10,11 +10,38 @@ namespace Tea {
 	class Input
 	{
 	public:
+		/**
+		 * Checks if a specific key is currently being pressed.
+		 *
+		 * @param key The key code of the key to check.
+		 * @return True if the key is currently being pressed, false otherwise.
+		 */
 		static bool IsKeyPressed(const KeyCode key);
 
+		/**
+		 * Checks if a mouse button is currently pressed.
+		 *
+		 * @param button The mouse button to check.
+		 * @return True if the mouse button is pressed, false otherwise.
+		 */
 		static bool IsMouseButtonPressed(const MouseCode button);
+		/**
+		 * Retrieves the current position of the mouse.
+		 *
+		 * @return The current position of the mouse as a 2D vector.
+		 */
 		static glm::vec2 GetMousePosition();
+		/**
+		 * @brief Retrieves the current x-coordinate of the mouse cursor.
+		 *
+		 * @return The x-coordinate of the mouse cursor.
+		 */
 		static float GetMouseX();
+		/**
+		 * @brief Retrieves the current y-coordinate of the mouse cursor.
+		 *
+		 * @return The y-coordinate of the mouse cursor.
+		 */
 		static float GetMouseY();
 	};
 }
