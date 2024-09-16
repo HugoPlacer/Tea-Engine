de38efa9e194c0a753b5633c58acb0513570b5b4
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-09-16 19:25:13 +0200

    ImGui group commented

--- TeaEngine/src/TeaEngine/ImGui/ImGuiLayer.h
+++ TeaEngine/src/TeaEngine/ImGui/ImGuiLayer.h
@@ -3,27 +3,78 @@
 #include "TeaEngine/Core/Layer.h"
 
 namespace Tea {
+
+    /**
+     * @defgroup imgui ImGui
+     * @{
+     */
+
+    /**
+     * @brief Layer for ImGui integration.
+     * @ingroup imgui
+     */
     class ImGuiLayer : public Layer
     {
     public:
+        /**
+         * @brief Constructor for ImGuiLayer.
+         */
         ImGuiLayer();
-        /// @brief
+
+        /**
+         * @brief Destructor for ImGuiLayer.
+         */
         ~ImGuiLayer();
 
+        /**
+         * @brief Called when the layer is attached.
+         */
         void OnAttach() override;
+
+        /**
+         * @brief Called when the layer is detached.
+         */
         void OnDetach() override;
+
+        /**
+         * @brief Called when an event occurs.
+         * @param e The event.
+         */
         void OnEvent(Event& e) override;
 
+        /**
+         * @brief Begin ImGui frame.
+         */
         void Begin();
-		void End();
 
+        /**
+         * @brief End ImGui frame.
+         */
+        void End();
+
+        /**
+         * @brief Called to render ImGui.
+         */
         void OnImGuiRender() override;
 
+        /**
+         * @brief Block or unblock events.
+         * @param block Whether to block events.
+         */
         void BlockEvents(bool block) { m_BlockEvents = block; }
 
+        /**
+         * @brief Set the Tea color style for ImGui.
+         */
         void SetTeaColorStyle();
+
+        /**
+         * @brief Set the Godot color style for ImGui.
+         */
         void SetGodotColorStyle();
     private:
-    	bool m_BlockEvents = true;
+        bool m_BlockEvents = true;
     };
+
+    /** @} */ // end of imgui group
 }
