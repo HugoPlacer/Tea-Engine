009e0923e4277f4ab382c57884f2dcc2cb638f00
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-09-16 19:33:22 +0200

    Scene Group Commented

--- TeaEngine/src/TeaEngine/Scene/SceneCamera.h
+++ TeaEngine/src/TeaEngine/Scene/SceneCamera.h
@@ -1,14 +1,33 @@
 #pragma once
 
 #include "TeaEngine/Renderer/Camera.h"
+
 namespace Tea {
 
+    /**
+     * @defgroup scene Scene
+     * @{
+     */
+
+    /**
+     * @brief Camera class for the scene.
+     * @ingroup scene
+     */
     class SceneCamera : public Camera
     {
     public:
+        /**
+         * @brief Constructor for SceneCamera.
+         */
         SceneCamera();
+
+        /**
+         * @brief Default destructor.
+         */
         ~SceneCamera() = default;
     private:
     };
+
+    /** @} */ // end of scene group
 
 }
